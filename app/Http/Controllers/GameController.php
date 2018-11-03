a69f4cf85c27f195a29c2fd9d87ab766ceceb317
Frank Pizzuta <frank@pizzuta.com>
2018-11-03 14:28:47 -0400

    try catch added to gamecontroller

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -57,7 +57,9 @@ class GameController extends Controller
         array_push($data, $request->all());
 //        dump($request->all());
 //        dump($data);
-        file_put_contents( database_path('/games.json'), json_encode($data));
+        try {
+            file_put_contents(database_path('/games.json'), json_encode($data));
+        } catch (\Exception $e) {};
 //        return redirect('games');
         return view('games.show')->with(['match' => $request]);
     }
